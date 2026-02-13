3bd61b691972a110548377f911132b7a5bbed0d8
Muigaidennis <denniskinuthiamuigai@gmail.com>
2026-02-13 23:08:04 +0300

    Add global CSS with Tailwind and body styles

---
+++ app/folder/global.css
@@ -0,0 +1,8 @@
+@tailwind base;
+@tailwind components;
+@tailwind utilities;
+
+body {
+  @apply bg-dark text-gray-100 min-h-screen;
+  font-family: system-ui, -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, sans-serif;
+}
